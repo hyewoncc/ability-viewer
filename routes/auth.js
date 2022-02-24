41fb6132788198933854b56b9628022f85b415e6
hyewoncc <hyewoncc@gmail.com>
2022-02-24 15:56:45 +0900

    fix: 로그인 endpoint 변경

--- routes/auth.js
+++ routes/auth.js
@@ -4,41 +4,5 @@ const api = require('../config/api');
 
 const { User } = require('../models/User');
 
-router.post('/', (req, res) => {
-    User.findOne({ id: req.body.id }, (err, user) => {
-      if(!user) {
-        return res.status(401).json({
-          loginSuccess: false,
-          message: "Unregistred id or invalid password"
-        });
-      }
-  
-      user.comparePassword(req.body.password, (err, isMatch) => {
-        if(!isMatch) {
-          return res.status(401).json({
-            loginSuccess: false,
-            message: "Unregistred id or invalid password"
-          });
-        }
-
-        user.generateToken((err, user) => {
-          if(err) {
-            return res.status(500).json({
-              loginSuccess: false,
-              message: "Server method failed"
-            });
-          }
-          return res.cookie("_auth", user.token)
-                    .status(200)
-                    .json({
-                      id: user.id,
-                      links: {
-                        user: api.url + 'users/' + user.id
-                      }
-                    });
-        })
-      })
-    })
-});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const api = require('../config/api');
+
+const { User } = require('../models/User');
 
 router.get('/', function(req, res, next) {
   res.json({
     message: "hello ability"
+  })
+});
+
+router.post('/login', (req, res) => {
+  User.findOne({ id: req.body.id }, (err, user) => {
+    if(!user) {
+      return res.status(401).json({
+        loginSuccess: false,
+        message: "Unregistred id or invalid password"
+      });
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) {
+        return res.status(401).json({
+          loginSuccess: false,
+          message: "Unregistred id or invalid password"
+        });
+      }
+
+      user.generateToken((err, user) => {
+        if(err) {
+          return res.status(500).json({
+            loginSuccess: false,
+            message: "Server method failed"
+          });
+        }
+        return res.cookie("_auth", user.token)
+                  .status(200)
+                  .json({
+                    id: user.id,
+                    links: {
+                      user: api.url + 'users/' + user.id
+                    }
+                  });
+      })
+    })
   })
 });
 

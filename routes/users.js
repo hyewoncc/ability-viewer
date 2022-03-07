9c64e4e46008140aa9a4c3d1d9d8b5efced18902
hyewoncc <hyewoncc@gmail.com>
2022-03-08 00:48:38 +0900

    feat: 회원 기본 정보 (책, 태그 조회 링크 포함)

--- routes/users.js
+++ routes/users.js
@@ -5,23 +5,39 @@ const api = require('../config/api');
 const { auth } = require('../middleware/auth');
 const { User } = require('../models/User');
 
-/* GET users listing. */
 router.get('/', auth, function(req, res, next) {
-  res.send('respond with a resource');
+    User.findById(req.user._id, function(err, user) {
+        if(err) {
+            return res.status(500).json({
+                message: "Server method failed"
+            })
+        }
+
+        return res.status(200).json({
+            user: {
+                id: user.id,
+                email: user.email,
+                links: {
+                    books: api.url + 'books',
+                    tags: api.url + 'tags'
+                }
+            }
+        })
+    })
 });
 
 router.post('/', function(req, res, next) {
-  const user = new User(req.body);
-  user.save((err, userInfo) => {
-    if(err) {
-      return res.status(500).json({ 
-        message: "Server method failed"
-      })
-    }
-    return res.status(201).json({
-      success: true
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if(err) {
+            return res.status(500).json({ 
+                message: "Server method failed"
+            })
+        }
+        return res.status(201).json({
+            success: true
+        })
     })
-  })
 });
 
 module.exports = router;

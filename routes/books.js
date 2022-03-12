1f558f4772db7ae4920ab4a2c0f3448db1e14d9d
hyewoncc <hyewoncc@gmail.com>
2022-03-13 03:20:01 +0900

    feat: 책 수정

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const res = require('express/lib/response');
 const router = express.Router();
 const api = require('../config/api');
 
@@ -168,6 +167,110 @@ router.post('/', auth, async(req, res, next) => {
             message: "Server method failed"
         })
     }
+})
+
+router.patch('/:_id', auth, async(req, res) => {
+    try {
+        const targetBook = await Book.findById(req.params._id);
+        const updateBook = req.body;
+
+        const user = await User.findById(req.user._id);
+
+        targetBook.name = updateBook.name;
+        targetBook.text = updateBook.text;
+
+        const updateTags = updateBook.tags;
+        const preserveTags = [];
+        const pullTagsId = [];
+
+        // 기존 : [아이돌, 현대, 전체]
+        // 수정 : [아이돌, 현대, bl, 수위]
+        for (const existTag of targetBook.tags) {
+            // 기존 태그가 수정 태그에 있는지 확인
+            let matchTag = updateTags.filter(tag => tag.name === existTag.name);
+
+            // 존재하지 않음 -> pullTagsId로 _id만
+            if (matchTag.length === 0) {
+                pullTagsId.push(existTag._id);
+            }
+        }
+
+        // 존재하지 않는 태그들을 book 에서 삭제
+        console.log(pullTagsId);
+        const tagPulledBook = await Book.findByIdAndUpdate(
+            { _id: targetBook._id }, 
+            { $pull: { tags: { _id: { $in: pullTagsId}}}}
+        );
+
+        // 기존 : [아이돌, 현대, 전체]
+        // 수정 : [아이돌, 현대, bl, 수위]
+        for (const updateTag of updateTags) {
+            // 수정 태그 중 기존 태그에 없던 것을 찾음
+            let isExist = false;
+            for (const oldTag of targetBook.tags ) {
+                if (updateTag.name === oldTag.name) {
+                    isExist = true;
+                }
+            }
+
+            // 새로운 태그라면 실행
+            if (!isExist) {
+                const originTag = await Tag.exists({user_id: user._id, name: updateTag.name});
+                if (!originTag) {
+                    const tag = new Tag(updateTag);
+                    tag.books.push(targetBook);
+                    tag.user_id = user._id;
+
+                    const newTag = await tag.save();
+                    targetBook.tags.push(newTag);
+                    user.tags.push(newTag);
+                }
+            }
+        }
+
+        // 삭제된 태그들에 이 책이 유일하다면 태그 삭제
+        for (const pullTagId of pullTagsId) {
+            const tag = await Tag.findById(pullTagId);
+            if (tag.books.length === 1) {
+                const deleteResult = await tag.delete();
+                const tagPulledUser = await User.findByIdAndUpdate(
+                        { _id: user._id },
+                        { $pull: { tags: { _id: tag._id }}}
+                    );
+            } else {
+                const updatedTag = await Tag.findByIdAndUpdate(
+                    { _id: pullTagId },
+                    { $pull: { books: { _id: targetBook._id }}}
+                )
+            }
+        }
+        
+        targetBook.save((err) => {
+            if(err) {
+                return res.status(500).json({
+                    message: "Server method failed"
+                })
+            }
+        })
+
+        user.save((err) => {
+            if(err) {
+                return res.status(500).json({
+                    message: "Server method failed"
+                })
+            }
+        })
+        
+    }catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: "Server method failed"
+        })
+    }
+    
+    return res.status(200).json({
+        success: true
+    })
 })
 
 router.delete('/:_id', auth, async(req, res) => {
